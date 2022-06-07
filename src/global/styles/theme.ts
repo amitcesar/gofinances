609fcf5d911de10f22e15771f27876218be16441
Amit cesar <amitcesar27@gmail.com>
2022-06-07 08:36:16 -0300

    subscript defaulttheme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,23 @@
+export default {
+  colors: {
+    PRIMARY: '#5636d3',
+
+    SECONDARY: '#FF872C',
+    SECONDARY_LIGHT: 'rgba(255, 135, 44, 0.3)',
+
+    SUCCESS: '#12A454',
+    SUCCESS_LIGHT: 'rgba(18, 164, 84, 0.5)',
+
+    ATTENTION: '#E83F5B',
+    ATTENTION_LIGHT: 'rgba(232, 63, 91, 0.5)',
+
+    WARNING: '#E7BA40',
+    
+    SHAPE: '#FFFFFF',
+    TITLE: '#363F5F',
+    TEXT : '#969CB2',
+
+    BACKGROUND: '#F0F2F5',
+
+  }
+}
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -4,6 +4,8 @@ export const Container = styled.View`
 flex: 1;
 justify-content: center;
 align-items: center;
+
+background-color: ${ (props) => props.theme.colors.PRIMARY};
 `
 
 export const Title = styled.Text`
