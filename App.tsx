db329ee6f629974769129ffe44cb7eb4d504d61b
Amit cesar <amitcesar27@gmail.com>
2022-06-07 08:17:52 -0300

    add styled-component and types

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/components/Welcome';
+import { Dashboard } from './src/screens/Dashboard';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Welcome title="Welcome to React Native with TypeScript!" />
+     <Dashboard />
     </View>
   );
 }
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {Container,Title} from './styles'
+
+export function Dashboard() {
+  return (
+   <Container>
+     <Title>
+       Dashboard
+     </Title>
+   </Container>
+  )
+}
