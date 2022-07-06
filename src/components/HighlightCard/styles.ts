ac335a6190655fe66882c0a31f06f3b59a386600
Amit cesar <amitcesar27@gmail.com>
2022-07-05 23:22:49 -0300

    feat: addtype and styles cardHighlight

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,10 +1,13 @@
-import styled from 'styled-components/native';
+import styled, {css} from 'styled-components/native';
 import {Feather} from '@expo/vector-icons'
 import { RFValue,RFPercentage } from 'react-native-responsive-fontsize';
 
+interface TypeProps {
+  type: 'up' | 'down' | 'total'
+}
 
-export const Container = styled.View`
-  background-color: ${ ({theme}) => theme.colors.SHAPE };
+export const Container = styled.View<TypeProps>`
+  background-color: ${ ({theme,type}) => type === 'total' ? theme.colors.SECONDARY : theme.colors.SHAPE };
   width: ${RFValue(300)}px;
   border-radius: 5px;
   padding: 19px 23px;
@@ -14,23 +17,47 @@ export const Container = styled.View`
 `;
 
 export const Header = styled.View`
-flex-direction: row;
-justify-content: space-between;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text<TypeProps>`
+  font-family: ${ ({theme}) => theme.fonts.REGULAR };
+  font-size: ${RFValue(14)}px;
+  color: ${ ({theme,type}) => type === 'total' ? theme.colors.SHAPE : theme.colors.TEXT };
+`;
 
-export const Icon = styled(Feather)`
- font-size: ${RFValue(40)}px;`;
+export const Icon = styled(Feather)<TypeProps>`
+ font-size: ${RFValue(40)}px;
+
+
+ ${({type}) => type === 'up' && css`
+ color: ${ ({theme}) => theme.colors.SUCCESS };
+ `}
+
+ ${({ type }) => type === 'down' && css`
+ color: ${ ({theme}) => theme.colors.ATTENTION };
+ `}
+
+ ${({type}) => type === 'total' && css`
+ color: ${ ({theme}) => theme.colors.SHAPE };
+ `}
+
+
+ `;
 
 export const Footer = styled.View``;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TypeProps>`
  font-family: ${ ({theme}) => theme.fonts.MEDIUM };
  font-size: ${RFValue(31)}px;
- color: ${ ({theme}) => theme.colors.TEXT_DARK };
+ color: ${ ({theme,type}) => type === 'total' ? theme.colors.SHAPE : theme.colors.TEXT_DARK };
 
  margin-top: 38px;
 `;
 
-export const LastTransaction = styled.Text``;
+export const LastTransaction = styled.Text<TypeProps>`
+  font-family: ${ ({theme}) => theme.fonts.REGULAR };
+ font-size: ${RFValue(12)}px;
+ color: ${ ({theme,type}) => type === 'total' ? theme.colors.SHAPE : theme.colors.TEXT };
+`;
